0c48fce2e5dab360ef074e1bf3a154401f3c1d42
Valery Baranov <v.baranov@integer-soft.ru>
2023-03-12 19:51:56 +0400

    add lib module

--- console-app/src/main/java/name/klamm/chess/helper/app/console/Main.java
+++ console-app/src/main/java/name/klamm/chess/helper/app/console/Main.java
@@ -1,7 +1,10 @@
 package name.klamm.chess.helper.app.console;
 
+import name.klamm.chess.helper.lib.Hello;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println(new Hello().hello());
     }
 }
---
+++ lib/src/main/java/name/klamm/chess/helper/lib/Hello.java
@@ -0,0 +1,7 @@
+package name.klamm.chess.helper.lib;
+
+public class Hello {
+    public String hello() {
+        return "hello";
+    }
+}
